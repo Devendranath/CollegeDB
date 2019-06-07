5c6a9548a499cc2cbbe7b8672cf5b1bcc054a385
Devendranath <dnreddy890@gmail.com>
2019-06-07 09:12:57 +0530

    Initial Commit

---
+++ CollegeDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollegeDB
+//
+//  Created by apple on 07/06/19.
+//  Copyright © 2019 iOSProofs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
